f282e43b73b292867aba4d6d7dcb7d36cf599e78
dellife <seheekwon21@gmail.com>
2020-03-21 22:13:56 +0900

    소스코드 추가

--- src/main/java/com/dellife/basicproject/web/BasicController.java
+++ src/main/java/com/dellife/basicproject/web/BasicController.java
@@ -16,4 +16,9 @@ public class BasicController {
     public String hello(BasicRequestDto requestDto) {
         return basicService.hello(requestDto.getAge());
     }
+
+    @GetMapping("/hello2")
+    public String hello2(BasicRequestDto requestDto) {
+        return basicService.hello(requestDto.getAge());
+    }
 }
